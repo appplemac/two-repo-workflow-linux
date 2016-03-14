a842f52b4041a89a9eebfca9a6cc4103d2d90708
Alexey <appplemac@gmail.com>
2016-03-14 18:09:35 -0400

    Fix typo in project name

--- trigger-os-x-build.sh
+++ trigger-os-x-build.sh
@@ -5,4 +5,4 @@ curl \
   --header "Content-Type: application/json" \
   --data '{"build_parameters": {"GIT_REVISION": "$CIRCLE_SHA1"}}' \
   --request POST \
-  "https://circleci.com/api/v1/project/appplemac/two-repo-workflow-osx/tree/master?circle-token=$CIRCLE_TOKEN"
+  "https://circleci.com/api/v1/project/appplemac/two-repo-workflow-os-x/tree/master?circle-token=$CIRCLE_TOKEN"
