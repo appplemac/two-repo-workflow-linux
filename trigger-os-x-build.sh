f22439d0f6a8eb2ac76660e0f540245fcd4ed32f
Alexey <appplemac@gmail.com>
2016-03-14 18:36:28 -0400

    Adjust variable name

--- trigger-os-x-build.sh
+++ trigger-os-x-build.sh
@@ -3,6 +3,6 @@ set -x
 
 curl \
   --header "Content-Type: application/json" \
-  --data "{\"build_parameters\": {\"GIT_REVISION\": \"$CIRCLE_SHA1\"}}" \
+  --data "{\"build_parameters\": {\"LINUX_SHA1\": \"$CIRCLE_SHA1\"}}" \
   --request POST \
   "https://circleci.com/api/v1/project/appplemac/two-repo-workflow-os-x/tree/master?circle-token=$CIRCLE_TOKEN"
